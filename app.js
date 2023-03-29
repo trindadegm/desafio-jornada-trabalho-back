67a4dab72cf85066853eb3bdeeb8352385af04d7
Gustavo Moitinho Trindade <gmoitinhotrindade@gmail.com>
2023-03-29 14:27:58 -0300

    Implement validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { body, validationResult } from 'express-validator'
 
 const port = 8000
 const app = express()
@@ -10,41 +11,33 @@ app.use(cors())
 app.use(express.json())
 
 var gSchedule = {
-  active: true,
-  configType: 'abort',
-  schedules: [
-    {
-      day: 1,
-      beginTime: '09:10',
-      endTime: '17:30',
-    },
-    {
-      day: 3,
-      beginTime: '09:10',
-      endTime: '17:30',
-    },
-    {
-      day: 5,
-      beginTime: '09:10',
-      endTime: '17:30',
-    },
-    {
-      day: 6,
-      beginTime: '14:00',
-      endTime: '16:00',
-    },
-  ],
+  active: false,
+  configType: 'sendInNextJourney',
+  schedules: [],
 }
 
 app.get('/schedule', (req, res) => {
   res.send(gSchedule)
 })
 
-app.put('/schedule', (req, res) => {
-  console.debug(`Request: ${JSON.stringify(req.body)}`)
-  gSchedule = req.body
-  res.status(200).send()
-})
+app.put(
+  '/schedule',
+  body('active').isBoolean(),
+  body('configType').isIn(['abort', 'sendInNextJourney']),
+  body('schedules.*.day').isIn([0, 1, 2, 3, 4, 5, 6]),
+  body('schedules.*.beginTime').matches("(2[0-3]|[0-1][0-9]):[0-5][0-9]"),
+  body('schedules.*.endTime').matches("(2[0-3]|[0-1][0-9]):[0-5][0-9]"),
+  (req, res) => {
+    console.debug(`Request: ${JSON.stringify(req.body)}`)
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() })
+    } else {
+      gSchedule = req.body
+      res.status(200).send()
+    }
+  }
+)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
